6a0bbde2694c7b93b3fd057fb80d8ccab30d3366
Tiagorg1 <tiagoramirog@gmail.com>
2023-04-01 22:39:46 -0300

    modificaciones

--- src/App.js
+++ src/App.js
@@ -2,13 +2,9 @@ import ItemListContainer from "./Components/ItemListContainer/ItemListContainer"
 import Navbar from "./Components/Navbar/Navbar";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Cart from "./Components/Cart/Cart";
-
 import ItemDetailContainer from "./Components/ItemDetailContainer/ItemDetailContainer";
-import Form from "./Form/Form";
 import CartContextProvider from "./Context/CartContext";
 function App() {
-  
-
   return (
     <BrowserRouter>
       <CartContextProvider>
@@ -21,7 +17,6 @@ function App() {
           />
           <Route path="/cart" element={<Cart />} />
           <Route path="ItemDetail/:id" element={<ItemDetailContainer />} />
-          <Route path="Form" element={<Form />} />
           <Route path="*" element={<h1> Error 404: Not found</h1>} />
         </Routes>
       </CartContextProvider>
